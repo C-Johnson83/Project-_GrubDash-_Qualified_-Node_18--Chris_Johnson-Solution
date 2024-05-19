8b958f55ac76333b66fb0408f0ac00973379c3de
chris <chrisj21283@gmail.com>
2024-05-19 12:09:07 -0400

    add create and read to dishes

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,3 +7,56 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+function create(req, res) {
+    const { data: { name, description, price, image_url } } = req.body;
+    const newDish = {
+        id: nextId(),
+        name: name,
+        description: description,
+        price: price,
+        image_url: image_url
+    };
+    dishes.push(newDish);
+    res.status(201).json({ data: newDish });
+}
+function hasDescription(req, res, next) {
+    const { data: { description } = {} } = req.body;
+  
+    if (description) {
+      return next();
+    }
+    next({ status: 404, message: "A 'description' property is required." });
+  }
+
+function list(req, res) {
+    res.json({ data: dishes });
+}
+function dishExists(req, res, next) {
+    const dishId = (req.params.dishId);
+    const foundDish = dishes.find((dish) => dish.id === dishId)
+    console.log(dishId, foundDish);
+    if (foundDish) {
+        res.locals.dish = foundDish
+    return next();
+}
+next({
+    status: 404,
+    message: `Dish not found: ${req.params.dishId}`
+});
+
+}
+
+
+function read(req, res) {
+    res.json({ data: res.locals.dish });
+}
+
+
+
+
+
+module.exports = {
+    create:[hasDescription, create],
+    list,
+    read:[dishExists, read]
+}
